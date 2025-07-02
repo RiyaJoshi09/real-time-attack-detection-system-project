82c61c96d0e0dc191274e82e406a0548555f1f01
Riya Joshi <127822580+RiyaJoshi09@users.noreply.github.com>
2025-07-02 06:55:46 +0000

    dockerised the backend

--- backend/app.py
+++ backend/app.py
@@ -3,17 +3,25 @@ from flask_socketio import SocketIO
 from routes import routes_blueprint, create_routes
 from socket_events import register_socket_events
 from config import Config
+import eventlet
+import os
+
+eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-socketio = SocketIO(app, cors_allowed_origins=Config.CORS_ALLOWED_ORIGINS, async_mode='threading')
-
+socketio = SocketIO(app, cors_allowed_origins=Config.CORS_ALLOWED_ORIGINS)
 
 # Register routes and WebSocket events
-create_routes(socketio)  # attach socketio to routes
+create_routes(socketio)
 app.register_blueprint(routes_blueprint)
 register_socket_events(socketio)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=Config.DEBUG)
+    # Get host and port from environment variables (Docker-friendly)
+    host = os.environ.get("FLASK_RUN_HOST", "0.0.0.0")
+    port = int(os.environ.get("FLASK_RUN_PORT", 5000))
+    
+    # Run the app with eventlet
+    socketio.run(app, host=host, port=port, debug=Config.DEBUG)
